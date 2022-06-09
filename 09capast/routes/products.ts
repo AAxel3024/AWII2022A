b665ce6106bdcfbcd505cb20f3efccb07dad81da
AAxel3024 <e1316854965@live.uleam.edu.ec>
2022-06-09 10:54:39 -0500

    09CAPAST2

---
+++ 09capast/routes/products.ts
@@ -0,0 +1,18 @@
+import{Router} from 'express'
+import{check} from 'express-validator'
+
+import {Producto} from '../controllers'
+
+const {crearProducto, obtenerProducto, obtenerProductos} = Producto
+
+import funciones from '../middlewares'
+
+const {validarCampos} = funciones
+
+const router = Router()
+
+router.get('/', obtenerProductos)
+router.get('/:id', check('id', 'Debe ser un Id vaido').isMongoId, validarCampos, obtenerProducto)
+router.post('/', check('nombre','El nombre es obligatorio').not().isEmpty(), validarCampos, crearProducto)
+
+export{router}
---
+++ 09capast/server.ts
@@ -0,0 +1,11 @@
+class Server 
+{
+    constructor(){}
+  private async conectarBD (){}
+   private middlewares(){}
+   private routes(){}
+   listen(){}
+
+}
+
+export {Server}
