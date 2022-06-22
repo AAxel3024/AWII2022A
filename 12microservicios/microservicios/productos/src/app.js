afbb9b186f09a2bd41f03147270dd09519fbc29f
AAxel3024 <e1316854965@live.uleam.edu.ec>
2022-06-22 15:19:48 -0500

    12Microservicios22/06/2022

---
+++ 12microservicios/microservicios/productos/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const respuesta = {
+    data : [],
+    arquitectura : 'Microservicio',
+    descripcion : 'Productos Microservicios'
+}
+
+
+app.get('/api/v2/productos' , (req,res) =>{
+    respuesta.data.push("Pizza","Hamburguesa");
+    console.log( "Microservicio Productos")
+    return res.send (respuesta);
+})
+
+module.exports = app;
---
+++ 12microservicios/microservicios/usuarios/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const respuesta = {
+    data : [],
+    arquitectura : 'Microservicio',
+    descripcion : 'Usuarios Micro'
+}
+
+
+app.get('/api/v2/usuarios' , (req,res) =>{
+    respuesta.data.push("Administrador","Superadministrador");
+    console.log( "Microservicio Clientes")
+    return res.send (respuesta);
+})
+
+module.exports = app;
